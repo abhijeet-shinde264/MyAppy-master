3eee12be7c528726f973e73d99397f5787211351
abhijeet-shinde264 <33342683+abhijeet-shinde264@users.noreply.github.com>
2020-04-08 17:07:35 +0530

    Initial commit

---
+++ app/src/main/java/com/andro/myappyy/Main3Activitycloud_retrieve.java
@@ -0,0 +1,13 @@
+package com.andro.myappyy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activitycloud_retrieve extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3_activitycloud_retrieve);
+    }
+}
